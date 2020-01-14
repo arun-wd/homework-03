2d1dcf4a6d868354a85e4b829280860afcd35179
RunS WD <anair8793@gmail.com>
2020-01-14 13:31:05 -0600

    last try

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homework-03
+
+tried a few different things but just haven't been very successful
--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var charLow = 'abcdefghijklmnopqrstuvwxyz';
+var char = 'abcdefghijklmnopqrstuvwxyz';
 var charCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var num = '0123456789';
 var sym = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-var characters = prompt("How many characters would you like to use?");
-var capital = confirm("Would you like capital letters?");
-var lower = confirm("Would you like lowercase letters?");
-var numbers = confirm("Would you like numbers?");
-var symbols = confirm("Would you like symbols?");
+var charNum = document.getElementById("#charNum");
+var capBox = document.getElementById("#cap");
+var numBox = document.getElementById("#num");
+var symBox = document.getElementById("#sym");
+var submit = document.getElementById("#generate");
+var yourPw = document.getElementById("#yourPw");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var length = characters.value,
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-    retVal = "";
-for (var i = 0, n = charset.length; i < length; ++i) {
-  retVal += charset.charAt(Math.floor(Math.random() * n));
+function password(l, characters) {
+  var pwd = '';
+  for(var i = 0; i<l; i++){
+    pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return pwd;
 }
-return retVal;
-passwordText.value = retVal;
-}
+
+
   copyBtn.removeAttribute("disabled");
   copyBtn.focus();
 }
@@ -33,6 +31,12 @@ function copyToClipboard() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+submit.addEventListener("click", function(e){
+  var characters = char;
+  (capBox.checked) ? characters += charCap : '';
+  (numBox.checked) ? characters += num : '';
+  (symBox.checked) ? characters += sym : '';
+  yourPw.value = password(charNum.value, characters);
+});
 
 // BONUS EVENT LISTENER
