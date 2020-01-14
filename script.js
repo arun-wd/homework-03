14ad1ca66263dda2c5a9f1d14687a3c82ea57070
RunS WD <anair8793@gmail.com>
2020-01-13 22:57:54 -0600

    trying after pwd changes

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charLow = 'abcdefghijklmnopqrstuvwxyz';
+var charCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var num = '0123456789';
+var sym = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+var characters = prompt("How many characters would you like to use?");
+var capital = confirm("Would you like capital letters?");
+var lower = confirm("Would you like lowercase letters?");
+var numbers = confirm("Would you like numbers?");
+var symbols = confirm("Would you like symbols?");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(l,characters) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  var pwd = '';
+  for (var i = 0; i<l; i++){
+    pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return pwd;
 
   passwordText.value = password;
 
